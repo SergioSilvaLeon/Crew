4b7e4201a9f5a96abc4e3a73c2fc71b033eb50b9
Sergio Silva <ssilva@nearsoft.com>
2019-07-01 16:03:59 -0700

    Initial commit

---
+++ src/main/java/com/nearsoft/crewtask/CrewTaskApplication.java
@@ -0,0 +1,13 @@
+package com.nearsoft.crewtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrewTaskApplication.class, args);
+    }
+
+}
