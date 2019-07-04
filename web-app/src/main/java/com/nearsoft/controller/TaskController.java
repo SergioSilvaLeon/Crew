6fc8eb7959be8df00e4aca45af0f5d11576beb34
Sergio Silva <ssilva@nearsoft.com>
2019-07-03 17:01:42 -0700

    Use thymeleaf for view

--- web-app/src/main/java/com/nearsoft/controller/TaskController.java
+++ web-app/src/main/java/com/nearsoft/controller/TaskController.java
@@ -1,18 +1,17 @@
 package com.nearsoft.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TaskController {
 
-    @RequestMapping(value = "/product", method = RequestMethod.GET)
-    public ModelAndView showForm() {
-        return new ModelAndView("productCreate", "product", new Product());
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 
 }
